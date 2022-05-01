619b97b5d7293103b0763809d5df5dbd2b839836
seryga-afk <seryga_94.94@mail.ru>
2022-05-01 13:06:33 +0300

    feat: finished the count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(catsArray) {
+  let cat = "^^"
+  let catsCount = 0
+  for (let i = 0; i < catsArray.length; i++) {
+    for (let j = 0; j < catsArray[i].length; j++) {
+      if (catsArray[i][j] === cat) {
+        catsCount++
+      }
+    }
+    
+  }
+  return catsCount
 }
 
 module.exports = {
